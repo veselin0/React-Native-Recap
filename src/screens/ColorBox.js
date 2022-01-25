c900fbcfe2b00fce1e79d5a4ce5043f1ae139fa3
Veselin <vesselin60@gmail.com>
2022-01-25 11:59:25 +0200

    Community convention in reducers

--- src/screens/ColorBox.js
+++ src/screens/ColorBox.js
@@ -6,19 +6,19 @@ import ColorSettings from '../components/ColorSettings';
 const COLOR_SETTING = 15;
 
 const reducer = (state, action) => {
-    switch (action.colorToChange) {
-        case 'red':
-            return state.red + action.amount > 255 || state.red + action.amount < 0 
+    switch (action.type) {
+        case 'change_red':
+            return state.red + action.payload > 255 || state.red + action.payload < 0 
             ? state
-            : {...state, red: state.red + action.amount};
-        case 'green':
-            return state.green + action.amount > 255 || state.green + action.amount < 0 
+            : {...state, red: state.red + action.payload};
+        case 'change_green':
+            return state.green + action.payload > 255 || state.green + action.payload < 0 
             ? state
-            : {...state, green: state.green + action.amount};
-        case 'blue':
-            return state.blue + action.amount > 255 || state.blue + action.amount < 0 
+            : {...state, green: state.green + action.payload};
+        case 'change_blue':
+            return state.blue + action.payload > 255 || state.blue + action.payload < 0 
             ? state
-            : {...state, blue: state.blue + action.amount};
+            : {...state, blue: state.blue + action.payload};
         default: 
             return state;           
     }
@@ -32,18 +32,18 @@ const ColorBox = () => {
   return (
     <View>
       <ColorSettings
-        onIncrease={() => dispatch({colorToChange: 'red', amount: COLOR_SETTING})}
-        onDecrease={() => dispatch({colorToChange: 'red', amount: -1 * COLOR_SETTING})}
+        onIncrease={() => dispatch({type: 'change_red', payload: COLOR_SETTING})}
+        onDecrease={() => dispatch({type: 'change_red', payload: -1 * COLOR_SETTING})}
         color="Red"
       />
       <ColorSettings
-        onIncrease={() => dispatch({colorToChange: 'green', amount: COLOR_SETTING})}
-        onDecrease={() => dispatch({colorToChange: 'green', amount: -1 * COLOR_SETTING})}
+        onIncrease={() => dispatch({type: 'change_green', payload: COLOR_SETTING})}
+        onDecrease={() => dispatch({type: 'change_green', payload: -1 * COLOR_SETTING})}
         color="Green"
       />
       <ColorSettings
-        onIncrease={() => dispatch({colorToChange: 'blue', amount: COLOR_SETTING})}
-        onDecrease={() => dispatch({colorToChange: 'blue', amount: -1 * COLOR_SETTING})}
+        onIncrease={() => dispatch({type: 'change_blue', payload: COLOR_SETTING})}
+        onDecrease={() => dispatch({type: 'change_blue', payload: -1 * COLOR_SETTING})}
         color="Blue"
       />
       <View
