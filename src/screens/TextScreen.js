2f44108c93b77d103b5dd514d10ca5c391568215
Veselin <vesselin60@gmail.com>
2022-01-26 09:09:20 +0200

    Two important props of TextInput

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -2,19 +2,23 @@ import React from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
 
 const TextScreen = () => {
-    return (
-        <View>
-            <TextInput style={styles.input} />
-        </View>
-    );
-}; 
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    input: {
-        margin: 15,
-        borderColor: 'black',
-        borderWidth: 1,
-    }
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
 });
 
 export default TextScreen;
