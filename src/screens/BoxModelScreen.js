cb6cbc6ff5f0dbc7f0edea2af8dec016da358328
Veselin <vesselin60@gmail.com>
2022-01-26 11:53:08 +0200

    Applying layout systems

--- src/screens/BoxModelScreen.js
+++ src/screens/BoxModelScreen.js
@@ -4,39 +4,37 @@ import {View, Text, StyleSheet} from 'react-native';
 const BoxModelScreen = () => {
     return (
         <View style={styles.viewStyle}>
-            <Text style={styles.textOneStyle}>Child #1</Text>
-            <Text style={styles.textTwoStyle}>Child #2</Text>
-            <Text style={styles.textThreeStyle}>Child #3</Text>
+            <View style={styles.viewOneStyle}></View>
+            <View style={styles.viewTwoStyle}></View>
+            <View style={styles.viewThreeStyle}></View>
         </View>
     );
 }; 
 
 const styles = StyleSheet.create({
     viewStyle: {
-       borderWidth: 3,
-       borderColor: 'black',
-        height: 200,
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      height: 160
     
     },
-    textOneStyle: {
-        borderWidth: 3,
-       borderColor: 'red',
+    viewOneStyle: {
+        backgroundColor: 'red',
+        height: 80,
+        width: 80
        
     },
-    textTwoStyle: {
-        borderWidth: 3,
-       borderColor: 'red',
-        fontSize: 18,
-    //     position: 'absolute',
-    //    top: 0,
-    //    right: 0,
-    //    bottom: 0,
-    //    left: 0
-    ...StyleSheet.absoluteFillObject
+    viewTwoStyle: {
+        backgroundColor: 'green',
+        height: 80,
+        width: 80,
+        alignSelf: 'flex-end',
+
     },
-    textThreeStyle: {
-        borderWidth: 3,
-       borderColor: 'red',
+    viewThreeStyle: {
+        backgroundColor: 'blue',
+        height: 80,
+        width: 80
      
     }
 });
