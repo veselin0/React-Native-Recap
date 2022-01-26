f63f3dc564106eafd7150875d4551a0d1c9f15c2
Veselin <vesselin60@gmail.com>
2022-01-26 08:36:58 +0200

    Refactor counter example changing useState for useReducer

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,21 +1,32 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'increase':
+      return {...state, counter: state.counter + action.payload};
+    case 'decrease': 
+      return {...state, counter: state.counter + action.payload};
+    default: return state;     
+  }
+}
+
 const CounterScreen = () => {
-  const [counter, setCounter] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {counter: 0});
+  const {counter} = state;
 
   return (
     <View>
       <Button
         title="Increase"
         onPress={() => {
-          setCounter(counter + 1);
+          dispatch({type: 'increase', payload: 1});
         }}
       />
       <Button
         title="Decrease"
         onPress={() => {
-          setCounter(counter - 1);
+          dispatch({type: 'decrease', payload: -1});
         }}
       />
       <Text>Current Count: {counter}</Text>
